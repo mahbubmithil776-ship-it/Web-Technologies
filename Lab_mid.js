280bca8eb3c8db4c1dc72a940e6d4e9eb901ac0b
mahbubmithil776-ship-it <mahbubmithil776@gmail.com>
2025-11-18 10:07:14 +0600

    Add files via upload

---
+++ Lab_mid.js
@@ -0,0 +1,6 @@
+const form = document.getElementById('Registation-form');
+const FullName = document.getElementById('Fullname');
+const Professional_email= document.getElementById('email');
+const Company = document.getElementById('Company');
+const Attendance = document.getElementById('Attendance'); 
+
